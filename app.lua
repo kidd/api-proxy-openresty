bf3544d7f0c4911d119cc9309efe1f406fda2813
Jose Galisteo <ceritium@gmail.com>
2015-10-26 19:58:35 +0100

    works

--- app.lua
+++ app.lua
@@ -1,17 +1,3 @@
-local redis = require "resty.redis"
-local red = redis:new()
-
-red:set_timeout(1000) -- 1 sec
-local ok, err = red:connect("192.168.99.100", 32768)
-
-
-local res, err = red:get('sbd:' .. ngx.var.subdomain)
-
-
-local rack = require 'rack'
-local r    = rack.new()
-
-r:use(require "my.middleware")
-local response = r:run()
-r:respond(response)
+ngx.say('Hello World!')
+ngx.exit(200)
 
