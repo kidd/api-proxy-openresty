baa5fe15b78516e1eba1227efd26216657a8bb75
Jose Galisteo <ceritium@gmail.com>
2015-10-26 21:04:29 +0100

    rate limit

--- app.lua
+++ app.lua
@@ -1,3 +1,13 @@
+local request = require "resty.rate.limit"
+request.limit {
+    key = ngx.var.remote_addr, rate = 2,
+    interval = 10,
+    log_level = ngx.NOTICE,
+    redis_config = { host = "127.0.0.1", port = 6379, timeout = 1, pool_size = 100 }
+  }
+
+
+
 ngx.say('Hello World!')
 ngx.exit(200)
 
