966c654a37a2ace16cee0fc3a74c80832be8f2ff
Raimon Grau <raimonster@gmail.com>
2017-11-22 16:55:33 +0000

    Add test addon to 'foo' service

--- seed-redis.sh
+++ seed-redis.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-redis-cli SET 'at:subdomain:bar' 'http://httpbin.org'
 redis-cli SET 'at:subdomain:foo' 'http://httpbin.org'
-redis-cli SET 'at:subdomain:foo' 'https://cardsagainsthumanity-api.herokuapp.com'
+redis-cli LPUSH 'at:addons:foo' 'test'
+#redis-cli SET 'at:subdomain:bar' 'http://httpbin.org'
+# redis-cli SET 'at:subdomain:foo' 'https://cardsagainsthumanity-api.herokuapp.com'
